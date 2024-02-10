2b4b1baf3c77140751e84e290f8e857a116333fc
kolya5544 <kolya@iktm.me>
2024-02-10 18:16:37 +0300

    Initial C# project creation

---
+++ MTUBankBase/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MTUBankBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
